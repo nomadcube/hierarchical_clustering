4f3d3e1a2cda2d035347d719cc7dae706743a4a7
西伯利亚游牧人 <nomadcube@gmail.com>
2015-10-28 15:42:17 +0800

    test

--- test_ward_heapq.py
+++ test_ward_heapq.py
@@ -2,7 +2,7 @@ import math
 
 import numpy as np
 
-from ward_heapq import Pair, Node, HierarchicalTree
+from ward_heapq import Node, HierarchicalTree, ClusterForest, Pair
 
 
 class TestPair:
@@ -13,50 +13,66 @@ class TestPair:
         assert p.val == 0
 
 
-class TestCluster:
+class TestNode:
     def test_init(self):
         a = np.array([1, 2])
         b = np.array([1, 2])
-        clu = Node([a, b])
+        clu = Node([a, b], 2)
         assert isinstance(clu.sample_data, np.ndarray)
-        assert len(clu.sample_data) == 2
+        assert clu.sample_data.shape == (2, 2)
 
     def test_len(self):
         a = np.array([1, 2])
         b = np.array([1, 2])
-        clu = Node([a, b])
+        clu = Node([a, b], 2)
         assert len(clu) == 2
 
     def test_variance(self):
         a = np.array([2, 2])
         b = np.array([0, 2])
-        clu = Node([a, b])
+        clu = Node([a, b], 2)
         assert clu.variance() == 1.0
-        a_clu = Node([a])
+        a_clu = Node([a], 1)
         assert a_clu.variance() == 0.0
 
     def test_union(self):
         a = np.array([2, 2])
         b = np.array([0, 2])
-        a_clu = Node([a])
-        b_clu = Node([b])
+        a_clu = Node([a], 1)
+        b_clu = Node([b], 1)
         c_clu = a_clu.union(b_clu)
-        assert len(c_clu) == 2
         assert c_clu.variance() == 1.0
         assert a_clu.ward(b_clu) == 1.0
 
 
 class TestHierarchicalTree:
-    def test_add_node(self):
-        ht = HierarchicalTree()
-        all_data = [np.array([k, k ** 2]) for k in range(3)]
-        for data in all_data:
-            ht.add_node(data)
-        assert len(ht) == 3
+    def test_init(self):
+        all_leaf = [np.array([k, k ** 2]) for k in range(3)]
+        ht = HierarchicalTree(all_leaf)
+        assert len(ht) == 2 * 3 - 1
+        assert len(ht._ward_heap_list) == 3
 
-    def test_ward_heapq(self):
-        ht = HierarchicalTree()
+    def test_node_ward(self):
         all_data = [np.array([k, k ** 2], dtype='int8') for k in range(3)]
-        for data in all_data:
-            ht.add_node(data)
-        assert isinstance(ht.ward_heapq(), list)
+        ht = HierarchicalTree(all_data)
+        i = 2 + 0
+        j = 2 + 1
+        w = ht._all_possible_nodes[i].ward(ht._all_possible_nodes[j])
+        assert isinstance(w, float)
+        assert w == 0.5
+
+    def test_heappop(self):
+        all_data = [np.array([k, k ** 2]) for k in range(3)]
+        ht = HierarchicalTree(all_data)
+        merge_pair = ht._nodes_to_be_merged()
+        assert isinstance(merge_pair, Pair)
+        assert merge_pair.i == 2
+        assert merge_pair.j == 3
+        assert merge_pair.val == 0.5
+
+
+class TestClusterForest:
+    def test_init(self):
+        all_data = [np.array([k, k ** 2]) for k in range(3)]
+        cf = ClusterForest(all_data)
+        assert cf.size() == 3
--- ward_heapq.py
+++ ward_heapq.py
@@ -1,6 +1,8 @@
-from heapq import heapify
+from heapq import heapify, heappop
 
 import numpy as np
+
+from tree_base import MutableLinkedBinaryTree
 
 
 class Pair:
@@ -14,67 +16,82 @@ class Pair:
 
 
 class Node:
-    def __init__(self, sample_data):
+    def __init__(self, sample_data, sample_size):
         try:
             if not isinstance(sample_data, np.ndarray):
                 raise TypeError()
             self.sample_data = sample_data
         except TypeError:
             self.sample_data = np.array(sample_data)
+        self.sample_data = self.sample_data.reshape((sample_size, -1))
 
     def variance(self):
-        n = len(self.sample_data)
-        sample_sum = 0
-        for sample in self.sample_data:
-            sample_sum += sample
-        sample_average = sample_sum / float(n)
-        tmp_variance = 0.0
-        for sample in self.sample_data:
-            tmp_variance += sum((sample - sample_average) ** 2)
-        return tmp_variance / float(n)
+        if self.sample_data.ndim == 1:
+            return 0.0
+        else:
+            n = self.sample_data.shape[0]
+            sample_sum = 0
+            for row_index in range(n):
+                sample_sum += self.sample_data[row_index]
+            sample_average = sample_sum / float(n)
+            tmp_variance = 0.0
+            for sample in self.sample_data:
+                tmp_variance += sum((sample - sample_average) ** 2)
+            return tmp_variance / float(n)
 
     def union(self, other):
         if not isinstance(other, Node):
             raise TypeError()
-        return Node(np.concatenate((self.sample_data, other.sample_data)))
+        return Node(np.concatenate((self.sample_data, other.sample_data)), len(self) + len(other))
 
     def __len__(self):
-        return len(self.sample_data)
+        return self.sample_data.shape[0]
 
     def ward(self, other):
         if not isinstance(other, Node):
             raise TypeError()
+        if not isinstance(other.sample_data, np.ndarray):
+            raise TypeError()
         new_cluster = self.union(other)
+        if not isinstance(new_cluster.sample_data, np.ndarray):
+            raise TypeError()
         return new_cluster.variance() - (self.variance() + other.variance())
 
 
 class HierarchicalTree:
-    def __init__(self):
-        self._all_nodes = []
-
-    def add_node(self, data):
-        self._all_nodes.append(Node(data))
+    def __init__(self, leaf_node_data):
+        self.n_leaf = len(leaf_node_data)
+        self._all_possible_nodes = [None] * (2 * self.n_leaf - 1)
+        for i in range(self.n_leaf):
+            self._all_possible_nodes[self.n_leaf - 1 + i] = Node(leaf_node_data[i], 1)
+        self._ward_heap_list = []
+        self._ward_heapify()
 
     def __len__(self):
-        return len(self._all_nodes)
+        return len(self._all_possible_nodes)
 
-    def nodes_to_be_merged(self):
-        pass
+    def _nodes_to_be_merged(self):
+        result = heappop(self._ward_heap_list)
+        return result
 
-    def ward_heapq(self):
-        # todo : this is not right.
-        heap_list = []
+    def _ward_heapify(self):
         for i in range(len(self)):
             for j in range(i + 1, len(self)):
-                heap_list.append(Pair(i, j, self._all_nodes[i].ward(self._all_nodes[j])))
-        heapify(heap_list)
-        return heap_list
+                node_i = self._all_possible_nodes[i]
+                node_j = self._all_possible_nodes[j]
+                if node_i is None or node_j is None:
+                    continue
+                self._ward_heap_list.append(Pair(i, j, node_i.ward(node_j)))
+        heapify(self._ward_heap_list)
 
 
-class MyList(list):
-    def __init__(self):
-        super(MyList, self).__init__()
-        print('TestList initiate OK.')
+class ClusterForest:
+    def __init__(self, leaf_data):
+        self._all_tree = []
+        for leaf in leaf_data:
+            new_tree = MutableLinkedBinaryTree()
+            new_tree._add_root(Node(leaf, 1))
+            self._all_tree.append(new_tree)
 
-if __name__ == '__main__':
-    tl = MyList()
+    def size(self):
+        return len(self._all_tree)
